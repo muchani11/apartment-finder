2882f90255889aed43310d173ee83b6258075cd9
muchani11 <muchani11@gmail.com>
2020-08-08 00:23:29 -0500

    environment variables

--- index.js
+++ index.js
@@ -24,10 +24,10 @@ const storage = multer.diskStorage({
 const upload = multer({storage: storage});
 
 var pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
+    host: process.env.RDS_HOSTNAME,
+    user: process.env.RDS_USERNAME,
+    password: process.env.RDS_PASSWORD,
+    database: process.env.RDS_DB_NAME,
     multipleStatements: true
 });
 
